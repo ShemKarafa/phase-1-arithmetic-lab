16aa81d030bd6c7ddafb7d4a645be572da645bcd
Shem Karafa <karafashem@gmail.com>
2024-03-23 17:39:33 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const setOfNumbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+console.log("max:", max); // Output: 20
